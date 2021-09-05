fd5f3c6d0948233523d9507ef36454e9cb620be2
Kalle Freij <freij.karl@gmail.com>
2021-09-05 21:51:44 +0200

    fix

--- ezCorrect/ezcorrect/src/teacher/assignments/assignments.actions.ts
+++ ezCorrect/ezcorrect/src/teacher/assignments/assignments.actions.ts
@@ -44,11 +44,10 @@ export const getAssignment = (id: string) => async (dispatch: any) => {
     dispatch({type: assignmentsActions.fetchAssignment});
     await axios.get('http://localhost:4000/api/assignments/single/' + id)
         .then((res: any) => {
-            console.log(res)
             dispatch({type: assignmentsActions.fetchAssignmentSuccessful});
-            let questionCards = [
+
+            let questionCards: ICreateTestQuestionCards[] = [
                 {id: '1', title: res.data.assignment.title, description: res.data.assignment.description, categories: [], subjects: [], cardType: 'header', isSelected: false},
-                {id: '2', question: '', questionType: '', cardType: 'question', isSelected: true, isDragDisabled: true, answer: null}
             ]
             let id = 3;
             res.data.assignment.questions.forEach((q: any) => {
@@ -56,11 +55,9 @@ export const getAssignment = (id: string) => async (dispatch: any) => {
                 questionCards.push({id: id.toString(), question: q.question, questionType: q.questionType, cardType: 'question', isSelected: false, isDragDisabled: true, answer: null})
                 id++
             });
-            //dispatch({type: assignmentsActions.editAssignment, payload: res.data.assignment.questions});
             dispatch({type: assignmentsActions.editAssignment, payload: questionCards});
         })
         .catch((err: any) => {
-            console.log(err.message)
             dispatch({type: assignmentsActions.fetchAssignmentFailed});
         })
 
