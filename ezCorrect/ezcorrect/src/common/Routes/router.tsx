0fd5d5f052aaa59d39c75de03b12dececc51455d
Kalle Freij <freij.karl@gmail.com>
2021-10-09 12:42:24 +0200

    student assignment folder

--- ezCorrect/ezcorrect/src/common/Routes/router.tsx
+++ ezCorrect/ezcorrect/src/common/Routes/router.tsx
@@ -15,6 +15,7 @@ import Confirm from '../signup/confirm';
 import StartHome from '../../start/home';
 import Navbar from '../navbar/navbar';
 import StudentHome from '../../student/home/components/home';
+import StartAssignment from '../../student/assignments/components/startAssignment';
 
 interface RouterProps {
   loggedIn: boolean;
@@ -28,10 +29,6 @@ interface RouterProps {
 }
 
 const Router: React.FC<RouterProps> = (props) => {
-  useEffect(() => {
-    console.log(props.loggedIn);
-    console.log(props.teacherAuth);
-  }, [props.loggedIn, props.teacherAuth]);
 
   return (
     <BrowserRouter>
@@ -57,6 +54,7 @@ const Router: React.FC<RouterProps> = (props) => {
         <ProtectedRoute exact path="/teacher/student" isAuthenticated={props.loggedIn && props.teacherAuth} component={Student} />
         <ProtectedRoute exact path="/teacher/profile" isAuthenticated={props.loggedIn && props.teacherAuth} component={Student} />
         <ProtectedRoute exact path="/student/home" isAuthenticated={props.loggedIn && props.studentAuth} component={StudentHome} />
+        <ProtectedRoute exact path="/student/assignment/start" isAuthenticated={props.loggedIn && props.studentAuth} component={StartAssignment} />
         <Route exact path="/home" render={() => <StartHome />} />
         <Route exact path="/signin" render={() => <SignIn onSignIn={props.onSignIn} />} />
         <Route exact path="/signup" render={() => <SignUp onSignUp={props.onSignUp} />} />
---
+++ ezCorrect/ezcorrect/src/student/assignments/components/startAssignment.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+const StartAssignment:React.FC = () => {
+
+    return (
+        <div>
+            <h1>Start Assignment</h1>
+        </div>  
+    )
+}
+
+export default StartAssignment;
