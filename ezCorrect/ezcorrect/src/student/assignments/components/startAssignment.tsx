8f5f84951d6e1b28229f2c44fef84596282c1ee4
Kalle Freij <freij.karl@gmail.com>
2021-10-10 18:53:12 +0200

    countdown to start assignment

--- ezCorrect/ezcorrect/src/student/assignments/components/startAssignment.tsx
+++ ezCorrect/ezcorrect/src/student/assignments/components/startAssignment.tsx
@@ -1,5 +1,7 @@
 import { createStyles, Grid, makeStyles, Theme } from '@material-ui/core';
 import * as React from 'react';
+import Countdown from 'react-countdown';
+import { NavLink } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -16,11 +18,19 @@ const useStyles = makeStyles((theme: Theme) =>
       clipPath: 'polygon(0 0, 0% 100%, 85% 50%);',
       color: '#fff',
       margin: '0 auto',
-      cursor: 'pointer',
       marginLeft: 50,
       '&:hover': {
         backgroundColor: '#6fa373',
       },
+    },
+    startButtonDisabled: {
+      backgroundColor: '#c1c1c1',
+      height: 250,
+      width: 250,
+      clipPath: 'polygon(0 0, 0% 100%, 85% 50%);',
+      color: '#fff',
+      margin: '0 auto',
+      marginLeft: 50
     },
     infoDiv: {
         border: 'solid red',
@@ -34,17 +44,30 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const StartAssignment:React.FC = () => {
     const classes = useStyles();
+    const [isAssignmentStartable, setIsAssignmentStartable] = React.useState<boolean>(false);
 
+    const handleStop = () => {
+      setIsAssignmentStartable(true)
+    }
     return (
         
         <div>
             <div className={classes.topDiv}/>
-            <Grid container direction="column" alignItems="center" spacing={8}>
+            <Grid container direction="column" alignItems="center" spacing={8} style={{width: '100%'}}>
                 <Grid item>
-                    <h1>Provet börjar om 30 min</h1>
+                  {isAssignmentStartable ? <NavLink to="/student/home" style={{textDecoration: 'none', color: 'black'}}><h1>Starta prov</h1></NavLink> : <h1>Provet börjar om: <Countdown date={Date.now() + 10000} onComplete={handleStop}/></h1>}
+                            
                 </Grid>
                 <Grid item>
-                    <div className={classes.startButton}/>
+                  {
+                    isAssignmentStartable ?
+                    <NavLink to="/student/home">
+                      <div className={classes.startButton}/>
+                    </NavLink>
+                    :
+                    <div className={classes.startButtonDisabled}/>
+                  }
+                  
                 </Grid>
                 <Grid item>
                     <div className={classes.infoDiv}>
